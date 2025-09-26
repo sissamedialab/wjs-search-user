2d5d521e1549ae08c0cc79dfb6b66198cdb45e69
Iacopo Spalletti <i.spalletti@nephila.it>
2025-09-26 10:16:05 +0200

    Emit event on body on autocomplete response
    Original typehead events are bound to the typehead element which is only created after the page is loaded and we can't attach a listener

--- wjs/user_search/static/js/typeahead.custom.js
+++ wjs/user_search/static/js/typeahead.custom.js
@@ -8,58 +8,55 @@
 // };
 
 function initTypeahead() {
-    document.querySelectorAll('.typeahead').forEach(input => {
-        if (input.dataset.initialized) return;
-        input.dataset.initialized = true;
+  document.querySelectorAll(".typeahead").forEach(input => {
+    if (input.dataset.initialized) return;
+    input.dataset.initialized = true;
 
-        var entity = input.dataset.entity || 'account';
-        var url = entity === 'collaboration'
-            ? '/searchapiget_collaboration/%QUERY'
-            : '/searchapiget/%QUERY';
+    const entity = input.dataset.entity || "account";
+    const url = entity === "collaboration"
+      ? "/searchapiget_collaboration/%QUERY"
+      : "/searchapiget/%QUERY";
 
-        var engine = new Bloodhound({
-            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            remote: { url: url, wildcard: '%QUERY' }
-        });
-        var minLength = 3;
-        function gatedSource(query, sync, async) {
-            if (query.length < minLength) {
-                return sync([]);
-            }
-            return engine.search(query, sync, async);
-        }
-
-        $(input).typeahead(null, {
-            source: gatedSource,
-            display: 'name',
-            name: entity,
-            minLength: 3,
-            limit: 41,
-            highlight: true,
-            templates: {
-                empty: `<div class="empty-message">no ${entity} found</div>`,
-                suggestion: Handlebars.compile('<div class="row"><span class="col">{{name}}</span><span class="col">{{email}}</span><span class="col">{{aff}}</span> <span class="col">{{orcid}}</span> <span class="col">{{country}}</span><span class="col-1 text-end">+</span></div>')
-            }
-        }).bind('typeahead:select', function(ev, suggestion) {
-    var targetEl;
-
-    if (input.dataset.entity === 'collaboration') {
-        targetEl = document.getElementById('id_collaboration_id');
-    } else {
-        targetEl = document.getElementById('id_author_id');
-    }
-
-    if (targetEl) {
-        targetEl.value = suggestion.id;
-        targetEl.dispatchEvent(new Event('change', { bubbles: true }));
-    }
-});
-
+    const engine = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name"),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      remote: { url: url, wildcard: "%QUERY" },
     });
+    const minLength = 3;
+
+    function gatedSource(query, sync, async) {
+      if (query.length < minLength) {
+        return sync([]);
+      }
+      return engine.search(query, sync, async);
+    }
+
+    $(input).typeahead(null, {
+      source: gatedSource,
+      display: "name",
+      name: entity,
+      minLength: 3,
+      limit: 41,
+      highlight: true,
+      templates: {
+        empty: `<div class="empty-message">no ${entity} found</div>`,
+        suggestion: Handlebars.compile("<div class=\"row\"><span class=\"col\">{{name}}</span><span class=\"col\">{{email}}</span><span class=\"col\">{{aff}}</span> <span class=\"col\">{{orcid}}</span> <span class=\"col\">{{country}}</span><span class=\"col-1 text-end\">+</span></div>"),
+      },
+    }).bind("typeahead:select", function(ev, suggestion) {
+      console.log(suggestion);
+      const targetEl = (input.dataset.entity === "collaboration") ? document.getElementById("id_collaboration_id") : document.getElementById("id_author_id");
+
+      if (targetEl) {
+        targetEl.value = suggestion.id;
+        targetEl.dispatchEvent(new Event("change", { bubbles: true }));
+      }
+    }).bind("typeahead:asyncreceive", function(ev) {
+      document.dispatchEvent(new Event("typeahead:asyncreceive", { bubbles: true }));
+    });
+  });
 }
 
-window.addEventListener('load', initTypeahead);
-document.body.addEventListener('htmx:afterSwap', function(evt) {
-    if (evt.target.querySelector('.typeahead')) initTypeahead();
+window.addEventListener("load", initTypeahead);
+document.body.addEventListener("htmx:afterSwap", function(evt) {
+  if (evt.target.querySelector(".typeahead")) initTypeahead();
 });
